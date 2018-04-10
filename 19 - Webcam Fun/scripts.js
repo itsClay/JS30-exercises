44cd69bb758db64918282a77f8192bf27cdfa379
Clay Shaw <Claydshaw@gmail.com>
2018-04-10 16:20:57 -0700

    partially complete, video and picture working

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,45 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio:false})
+    .then(localMediaStream => {
+      // console.log(localMediaStream)
+      try {
+        video.srcObject = localMediaStream
+      } catch (err) {
+        console.error(err)
+        video.src = window.URL.createObjectURL(localMediaStream)
+      }
+      video.play()
+    })
+    .catch(e => {
+      console.error('hmm somethings not right', e)
+    })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth
+  const height = video.videoHeight
+  console.log(width, height)
+  canvas.height = height
+  canvas.width = width
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height)
+  }, 16)
+}
+
+function takePhoto() {
+  // play sound
+  snap.currentTime = 0
+  snap.play()
+
+  // take data out of the canvas
+  const data = canvas.toDataUrl('image/jpeg')
+}
+
+getVideo()
+
+video.addEventListener('canplay', paintToCanvas)
